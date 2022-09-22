cc1b1d0f7b4a6194ab56e4194149644c8be45019
pablo <pablovaldesgaete23@gmail.com>
2022-09-22 10:59:41 -0300

    versionfinal

--- pava_app2/views.py
+++ pava_app2/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def displayApp2(request):
-    return HttpResponse("<h1>Gracias por Partipar en esta segunda vista</h1>")
+    return HttpResponse("<h1>ya que logro llegar aquí ¡¡¡¡Felicidades¡¡¡¡</h1>")
                         
+def displayDateTime(request):
+    dt = datetime.datetime.now()
+    s = "<b> Fecha y Hora Actual: </b>" + str(dt)
+    return HttpResponse(s)
